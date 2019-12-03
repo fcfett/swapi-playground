3da42e4a3967a37e7fc11e56013b29b348eb045f
Felipe Fett <felipe.fett@opngo.com.br>
2019-12-03 17:02:57 -0300

    Crawling intro

--- src/components/molecules/Film/index.jsx
+++ src/components/molecules/Film/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './style.scss';
 
 import Thumb from '../Thumb';
@@ -6,10 +6,37 @@ import Icon from '../../atoms/Icon';
 
 export default ({ date, alt }) => {
   const [isActive, setActive] = useState(false);
+  const [scrolling, setScrolling] = useState();
+
   const toggleActive = (toggle) => {
     console.warn('toggleActive', toggle);
     setActive(toggle ? toggle : !isActive);
   };
+
+  const introRef = useRef();
+  useEffect(() => {
+    triggerCrawling();
+  }, [isActive]); // eslint-disable-line
+
+  const triggerCrawling = () => {
+    const introEl = introRef.current;
+    clearInterval(scrolling);
+    if (isActive) {
+      introEl.scrollTop = 0;
+      const interval = setInterval(() => {
+        introEl.scrollTop += 2;
+        const { offsetHeight, scrollTop, scrollHeight } = introEl;
+        console.warn(`scrolling ${scrollTop} of ${scrollHeight - offsetHeight}`);
+        if (offsetHeight + scrollTop === scrollHeight) {
+          console.warn('clearInterval');
+          clearInterval(interval);
+          setScrolling(null);
+        }
+      }, 40);
+      setScrolling(interval);
+    }
+  };
+
   return (
     <li className={`card ${isActive ? 'active' : ''}`}>
       <Thumb date={date} alt={alt} onClick={() => toggleActive(true)} />
@@ -18,16 +45,22 @@ export default ({ date, alt }) => {
           <Icon name="close" height={32} width={32} />
         </button>
         <article className="intro">
-          {`It is a period of civil war.\r\nRebel spaceships, striking\r\nfrom a hidden base, have won\r\ntheir first
+          <section className="crawler" ref={introRef}>
+            <p className="text">{`It is a period of civil war.\r\nRebel spaceships, striking\r\nfrom a hidden base, have won\r\ntheir first
           victory against\r\nthe evil Galactic Empire.\r\n\r\nDuring the battle, Rebel\r\nspies managed to steal
           secret\r\nplans to the Empire's\r\nultimate weapon, the DEATH\r\nSTAR, an armored space\r\nstation with enough
           power\r\nto destroy an entire planet.\r\n\r\nPursued by the Empire's\r\nsinister agents, Princess\r\nLeia
           races home aboard her\r\nstarship, custodian of the\r\nstolen plans that can save her\r\npeople and
-          restore\r\nfreedom to the galaxy....`}
+          restore\r\nfreedom to the galaxy....`}</p>
+          </section>
+          <button className={`btn-replay ${(!scrolling && 'visible') || ''}`} onClick={() => triggerCrawling()}>
+            <Icon name="reload" width={60} height={60} />
+          </button>
         </article>
         <article className="info">
-          <h1>Episode I - A New Hope</h1>
-          <p>Release Date: {date}</p>
+          <h2>Episode I</h2>
+          <h1>A New Hope</h1>
+          <p>{date}</p>
           <p>Directed by George Lucas</p>
           <p>Produced by Gary Kurtz, Rick McCallum</p>
           <ul>
--- src/components/molecules/SvgDefs/index.jsx
+++ src/components/molecules/SvgDefs/index.jsx
@@ -8,6 +8,7 @@ import { ReactComponent as Starships } from '../../../assets/icons/starships.svg
 import { ReactComponent as Vehicles } from '../../../assets/icons/vehicles.svg';
 import { ReactComponent as Plus } from '../../../assets/icons/plus.svg';
 import { ReactComponent as AngleDown } from '../../../assets/icons/angle-down.svg';
+import { ReactComponent as Reload } from '../../../assets/icons/reload.svg';
 
 export default (props) => (
   <svg id="svg-defs">
@@ -49,6 +50,10 @@ export default (props) => (
 
     <symbol id="svg-icon-prev">
       <AngleDown />
+    </symbol>
+
+    <symbol id="svg-icon-reload">
+      <Reload />
     </symbol>
   </svg>
 );
