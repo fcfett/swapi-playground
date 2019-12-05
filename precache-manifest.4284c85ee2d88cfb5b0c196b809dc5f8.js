b2e9855a33d43511afbdf555462b19cfa0d89018
Felipe Fett <felipe.fett@opngo.com.br>
2019-12-05 20:13:35 -0300

    Updates

---
+++ precache-manifest.4284c85ee2d88cfb5b0c196b809dc5f8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "528dbe867ad0ed7ffb8923d041906115",
+    "url": "/swapi-playground/index.html"
+  },
+  {
+    "revision": "5c6216834eac4795db20",
+    "url": "/swapi-playground/static/css/main.c66db85d.chunk.css"
+  },
+  {
+    "revision": "703763532abe24fb8290",
+    "url": "/swapi-playground/static/js/2.1f157424.chunk.js"
+  },
+  {
+    "revision": "5c6216834eac4795db20",
+    "url": "/swapi-playground/static/js/main.1ef8ca00.chunk.js"
+  },
+  {
+    "revision": "86054da5a8d82428f6ec",
+    "url": "/swapi-playground/static/js/runtime-main.b982d893.js"
+  },
+  {
+    "revision": "1d8abd571b1b70f0dace6eb25ca75fdf",
+    "url": "/swapi-playground/static/media/1977-05-25.1d8abd57.jpeg"
+  },
+  {
+    "revision": "b44fc018700d16cdc93cca2d89286043",
+    "url": "/swapi-playground/static/media/1980-05-17.b44fc018.jpeg"
+  },
+  {
+    "revision": "a87381ba56adad6739ef1a181230edff",
+    "url": "/swapi-playground/static/media/1983-05-25.a87381ba.jpeg"
+  },
+  {
+    "revision": "5fa9e2b65273b02b821c32881162d2c3",
+    "url": "/swapi-playground/static/media/1999-05-19.5fa9e2b6.jpeg"
+  },
+  {
+    "revision": "e8cd32c7e50c08042a3f3c4afcefd9f1",
+    "url": "/swapi-playground/static/media/2002-05-16.e8cd32c7.jpeg"
+  },
+  {
+    "revision": "96da60b6f3f30dd2ec06d484b967774d",
+    "url": "/swapi-playground/static/media/2005-05-19.96da60b6.jpeg"
+  },
+  {
+    "revision": "a31a9bcf0c2f00aefe8fc72d72008e99",
+    "url": "/swapi-playground/static/media/2015-12-11.a31a9bcf.jpeg"
+  },
+  {
+    "revision": "153b3c9b099e0d5c8e4344033f397665",
+    "url": "/swapi-playground/static/media/2017-12-14.153b3c9b.jpeg"
+  },
+  {
+    "revision": "56c8ef7f5333f94ffdd7a0e5ddcb4475",
+    "url": "/swapi-playground/static/media/angle-down.56c8ef7f.svg"
+  },
+  {
+    "revision": "7fc75b3979a7370b01cc97347a793274",
+    "url": "/swapi-playground/static/media/background.7fc75b39.jpg"
+  },
+  {
+    "revision": "776608fc37d227c4b7889e401c903b81",
+    "url": "/swapi-playground/static/media/brushstrike.776608fc.ttf"
+  },
+  {
+    "revision": "125a8bb452d2427ffc6ce229777ffdda",
+    "url": "/swapi-playground/static/media/logo-fill.125a8bb4.svg"
+  },
+  {
+    "revision": "a1148ea0ec5e18cecfb9ac8aa6944d06",
+    "url": "/swapi-playground/static/media/plus.a1148ea0.svg"
+  },
+  {
+    "revision": "212839c04ff092dc8d8e013933e97ae9",
+    "url": "/swapi-playground/static/media/reload.212839c0.svg"
+  }
+]);
